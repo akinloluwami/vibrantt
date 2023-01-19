4d7ecfcefee467fd97428e391451b5dd2fa4e11e
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-19 11:49:59 +0100

    ...

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,18 @@
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="w-screen h-screen">
+      <div className="flex shadow-sm items-center justify-between px-3 h-[10%]">
+        <h1>Vibrantt</h1>
+        <button>Follow on Twitter</button>
+      </div>
+      <div className="flex items-center justify-between h-[90%] bg-gray-600">
+        <div className=" h-full w-[20%] bg-red-500"></div>
+        <div className=" h-full w-[20%] bg-green-500"></div>
+        <div className=" h-full w-[20%] bg-yellow-500"></div>
+        <div className=" h-full w-[20%] bg-orange-500"></div>
+        <div className=" h-full w-[20%] bg-purple-500"></div>
+      </div>
+      {/* <div className="h-[10%]"></div> */}
+    </div>
+  );
 }
