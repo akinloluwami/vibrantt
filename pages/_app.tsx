e7ec5ce58992687923e0e337397eb96b05717954
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-19 13:49:01 +0100

    added favicon and page title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link
+          rel="shortcut icon"
+          href="https://res.cloudinary.com/xing0x/image/upload/v1674131508/VIBRANT_icon_nrwiko.png"
+          type="image/x-icon"
+        />
+        <title>Vibrantt - Color Palette Generator</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
