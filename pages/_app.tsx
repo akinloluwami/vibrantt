7fee9270a1d2e46512aaaf6119118ee4c4ea3c54
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-19 15:03:48 +0100

    more og stuff

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,21 @@ export default function App({ Component, pageProps }: AppProps) {
           type="image/x-icon"
         />
         <title>Vibrantt - Color Palette Generator</title>
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/xing0x/image/upload/v1674131508/VIBRANT_icon_nrwiko.png"
+        ></meta>
+        <meta name="description" content="Color Palette Generator"></meta>
+
+        <meta
+          property="twitter:image"
+          content="https://res.cloudinary.com/xing0x/image/upload/v1674131508/VIBRANT_icon_nrwiko.png"
+        ></meta>
+
+        <meta
+          name="twitter:description"
+          content="Color Palette Generator"
+        ></meta>
       </Head>
       <Component {...pageProps} />
     </>
