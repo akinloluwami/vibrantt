96a183f452ab2a334ad6e36cf412d2885aeb7553
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-19 15:32:58 +0100

    set up google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import * as gtag from "../utils/gtag";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <>
       <Head>
