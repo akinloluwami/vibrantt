c7a94ef4a3a2ae7d2f08cb036d072c13b026fe01
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-19 12:37:21 +0100

    added hex to rgb function

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,13 @@ export default function Home() {
     setColors(randomColor({ count: 5 }));
   };
 
+  function hexToRgb(hex: string) {
+    const r = parseInt(hex.slice(1, 3), 16);
+    const g = parseInt(hex.slice(3, 5), 16);
+    const b = parseInt(hex.slice(5, 7), 16);
+    return { r, g, b };
+  }
+
   const getLuminosity = (hex: string) => {
     const r = parseInt(hex.substring(0, 2), 16);
     const g = parseInt(hex.substring(2, 4), 16);
