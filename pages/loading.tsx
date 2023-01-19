fd4e28072ba1e4da6e74184c8dd4092b2bbc8bd6
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-19 23:19:43 +0100

    loading comp

--- pages/loading.tsx
+++ pages/loading.tsx
@@ -1,7 +1,28 @@
 import React from "react";
 
-const loading = () => {
-  return <div>loading...</div>;
+const Loading = () => {
+  return (
+    <div
+      className="flex items-center justify-start lg:h-[90%] h-[84%] lg:flex-row flex-col animate-pulse"
+      role="status"
+    >
+      <div
+        className={`lg:h-full h-[20%] lg:w-[20%]  w-full flex items-center justify-center bg-slate-300`}
+      ></div>
+      <div
+        className={`lg:h-full h-[20%] lg:w-[20%]  w-full flex items-center justify-center bg-slate-400`}
+      ></div>
+      <div
+        className={`lg:h-full h-[20%] lg:w-[20%]  w-full flex items-center justify-center bg-slate-500`}
+      ></div>
+      <div
+        className={`lg:h-full h-[20%] lg:w-[20%]  w-full flex items-center justify-center bg-slate-600`}
+      ></div>
+      <div
+        className={`lg:h-full h-[20%] lg:w-[20%]  w-full flex items-center justify-center bg-slate-700`}
+      ></div>
+    </div>
+  );
 };
 
-export default loading;
+export default Loading;
