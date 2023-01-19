474e3984c860d28d1a5a6dba89f04ee47895411d
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-19 22:47:38 +0100

    wo

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 declare module "@yatiac/name-that-color";
-
+declare module "nearest-color";
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
