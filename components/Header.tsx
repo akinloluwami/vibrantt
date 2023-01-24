6c38e874203f5c90da5f9810eae678f483a911c4
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-24 12:48:38 +0100

    ...

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,8 @@
 import Link from "next/link";
 import { SiTwitter } from "react-icons/si";
+import { TiArrowBack, TiArrowForward } from "react-icons/ti";
 
-const Header = () => {
+const Header = ({ undo, currentIndex, redo, prevPalettes }: any) => {
   return (
     <>
       <img
@@ -22,6 +23,22 @@ const Header = () => {
         <SiTwitter />
         Follow on Twitter
       </Link>
+      <div className="flex items-center gap-2">
+        <button
+          className="btn rounded-full text-lg"
+          onClick={undo}
+          disabled={currentIndex === 1}
+        >
+          <TiArrowBack />
+        </button>
+        <button
+          className="btn rounded-full text-lg"
+          onClick={redo}
+          disabled={currentIndex === prevPalettes.length - 1}
+        >
+          <TiArrowForward />
+        </button>
+      </div>
     </>
   );
 };
