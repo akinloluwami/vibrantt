783eb469b233b34171501eaa65b078ce1f87a7ba
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-24 11:58:11 +0100

    separated the header component

---
+++ components/Header.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import React from "react";
+import { SiTwitter } from "react-icons/si";
+
+const Header = () => {
+  return (
+    <>
+      <img
+        src="https://res.cloudinary.com/xing0x/image/upload/v1674131509/VIBRANTwbgggw_w6j7m9.png"
+        className="w-32"
+        alt=""
+      />
+
+      <p className="text-xl text-center lg:block hidden">
+        Press the space bar to generate a new palette!
+      </p>
+
+      <Link
+        href="https://twitter.com/vibranttdotco/"
+        target={"_blank"}
+        className="flex items-center gap-2"
+      >
+        <SiTwitter />
+        Follow on Twitter
+      </Link>
+    </>
+  );
+};
+
+export default Header;
