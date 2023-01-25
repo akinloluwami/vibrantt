5b92085b0503237083c2374aad74f324978bd84c
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-25 13:44:08 +0100

    set color spaces

--- components/Drawer.tsx
+++ components/Drawer.tsx
@@ -2,9 +2,12 @@ import Button from "@/component-elements/Button";
 import useDrawerStore, { DrawerContext } from "@/stores/useDrawerStore";
 import { X } from "lucide-react";
 import { useState } from "react";
+import Toggle from "./Toggle";
 
 const Drawer = () => {
   const { isOpen, close } = useDrawerStore();
+
+  const colorSpaces = ["HEX", "RGB", "HSL"];
   return (
     <div
       className={`w-72 h-full shadow-2xl absolute bg-[#3e4452] right-0 ${
@@ -16,6 +19,22 @@ const Drawer = () => {
         <Button onClick={close}>
           <X />
         </Button>
+      </div>
+      <div className="w-full mt-5">
+        <div className="flex items-center gap-2 w-full my-5">
+          <p className="font-semibold">Color space</p>
+          <select name="" id="" className="flex-1 py-0 bg-[#2a303c]">
+            {colorSpaces.map((colorSpace, i) => (
+              <option value={colorSpace} key={i}>
+                {colorSpace}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="flex items-center gap-2 w-full">
+          <p className="font-semibold">Color names</p>
+          <Toggle />
+        </div>
       </div>
     </div>
   );
