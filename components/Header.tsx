62333b9a0fef751193b05706aec23ad96e7b5e54
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-25 11:04:19 +0100

    stuff

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { SiTwitter } from "react-icons/si";
 import { TiArrowBack, TiArrowForward } from "react-icons/ti";
+import { Settings2 } from "lucide-react";
 
 const Header = ({ undo, currentIndex, redo, prevPalettes }: any) => {
   return (
@@ -25,20 +26,23 @@ const Header = ({ undo, currentIndex, redo, prevPalettes }: any) => {
       </Link>
 
       <div className="lg:flex items-center gap-2 hidden lg:visible">
-        <button
-          className="btn rounded-full text-lg"
-          onClick={undo}
-          disabled={currentIndex === 1}
-        >
-          <TiArrowBack />
-        </button>
-        <button
-          className="btn rounded-full text-lg"
-          onClick={redo}
-          disabled={currentIndex === prevPalettes.length - 1}
-        >
-          <TiArrowForward />
-        </button>
+        <div className="flex items-center gap-2">
+          <button
+            className="btn rounded-full text-lg bg-transparent outline-none border-none"
+            onClick={undo}
+            disabled={currentIndex === 1}
+          >
+            <TiArrowBack />
+          </button>
+          <button
+            className="btn rounded-full text-lg"
+            onClick={redo}
+            disabled={currentIndex === prevPalettes.length - 1}
+          >
+            <TiArrowForward />
+          </button>
+        </div>
+        <Settings2 />
       </div>
     </>
   );
