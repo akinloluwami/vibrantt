cdec1f1a4003239c7706f4078c063cbbc8814282
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-25 14:03:05 +0100

    transition

--- components/Toggle.tsx
+++ components/Toggle.tsx
@@ -1,15 +1,15 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 const Toggle = () => {
-  const [on, setOn] = useState(false);
+  const [on, setOn] = useState(true);
   return (
     <div
       className="flex items-center w-10 bg-red-50 rounded-full relative h-5 cursor-pointer"
       onClick={() => setOn(!on)}
     >
       <div
-        className={`bg-red-500 h-4 w-4 rounded-full absolute ${
-          on ? "right-[2px]" : "left-[2px]"
+        className={`bg-red-500 h-4 w-4 rounded-full absolute transition-all ${
+          on ? "translate-x-[22px]" : "translate-x-[2px]"
         } `}
       ></div>
     </div>
