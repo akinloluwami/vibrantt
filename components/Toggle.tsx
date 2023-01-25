8a97764279581f5ddfa6095eccdaa861cc6d6a9c
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-25 13:55:20 +0100

    added state change

--- components/Toggle.tsx
+++ components/Toggle.tsx
@@ -3,7 +3,10 @@ import React, { useState } from "react";
 const Toggle = () => {
   const [on, setOn] = useState(false);
   return (
-    <div className="flex items-center w-10 bg-red-50 rounded-full relative h-5">
+    <div
+      className="flex items-center w-10 bg-red-50 rounded-full relative h-5"
+      onClick={() => setOn(!on)}
+    >
       <div
         className={`bg-red-500 h-4 w-4 rounded-full absolute ${
           on ? "right-[2px]" : "left-[2px]"
