f08a6f31ca70b1e2e6bbcb2ed8e011c2c131a232
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-25 20:58:23 +0100

    @@

--- components/Toggle.tsx
+++ components/Toggle.tsx
@@ -1,15 +1,19 @@
 import { useState } from "react";
 
-const Toggle = () => {
-  const [on, setOn] = useState(true);
+interface ToggleType {
+  isOn: boolean;
+  onClick: () => void;
+}
+
+const Toggle = ({ isOn, onClick }: ToggleType) => {
   return (
     <div
       className="flex items-center w-10 bg-[#2a303c] rounded-full relative h-5 cursor-pointer"
-      onClick={() => setOn(!on)}
+      onClick={onClick}
     >
       <div
         className={`bg-gray-400 h-4 w-4 rounded-full absolute transition-all ${
-          on ? "translate-x-[22px]" : "translate-x-[2px]"
+          isOn ? "translate-x-[22px]" : "translate-x-[2px]"
         } `}
       ></div>
     </div>
