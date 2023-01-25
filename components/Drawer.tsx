4275794e2c53e982e37a8aec5498b6ae5356f82b
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-25 12:42:50 +0100

    progress

--- components/Drawer.tsx
+++ components/Drawer.tsx
@@ -1,30 +1,27 @@
 import Button from "@/component-elements/Button";
-import useDrawerStore from "@/stores/useDrawerStore";
+import useDrawerStore, { DrawerContext } from "@/stores/useDrawerStore";
 import { X } from "lucide-react";
 import { useState } from "react";
 
 const Drawer = () => {
   const { isOpen, open, close } = useDrawerStore();
   return (
-    <div
-      className={`w-72 h-full shadow-2xl absolute bg-[#3e4452] right-0 ${
-        isOpen ? "translate-x-0" : "translate-x-full"
-      } z-[10000000] px-2`}
-    >
-      <div className="flex items-center py-1 justify-between">
-        <h1 className="font-semibold text-2xl">
-          {isOpen ? "Settings" : "Blu"}
-        </h1>
-        <Button
-          onClick={() => {
-            console.log("x");
-            isOpen ? close() : open();
-          }}
-        >
-          <X />
-        </Button>
+    <DrawerContext.Provider value={{ isOpen, open, close }}>
+      <div
+        className={`w-72 h-full shadow-2xl absolute bg-[#3e4452] left-0 ${
+          isOpen ? "translate-x-0" : "translate-x-full"
+        } z-[10000000] px-2 transition-all`}
+      >
+        <div className="flex items-center py-1 justify-between">
+          <h1 className="font-semibold text-2xl">
+            {isOpen ? "Settings" : "Blu"}
+          </h1>
+          <Button onClick={close}>
+            <X />
+          </Button>
+        </div>
       </div>
-    </div>
+    </DrawerContext.Provider>
   );
 };
 
