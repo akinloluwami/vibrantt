baa06c1dee9fe0ad28ff5e08a92c83549a677a35
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-25 13:17:32 +0100

    fixed

--- components/Drawer.tsx
+++ components/Drawer.tsx
@@ -4,15 +4,15 @@ import { X } from "lucide-react";
 import { useState } from "react";
 
 const Drawer = () => {
-  const { isOpen, open, close } = useDrawerStore();
+  const { isOpen, close } = useDrawerStore();
   return (
     <div
       className={`w-72 h-full shadow-2xl absolute bg-[#3e4452] right-0 ${
         isOpen ? "translate-x-0" : "translate-x-full"
       } z-[10000000] px-2 transition-all`}
     >
-      <div className="flex items-center py-1 justify-between">
-        <h1 className="font-semibold text-2xl">Setting</h1>
+      <div className="flex items-center justify-between h-[10%]">
+        <h1 className="font-semibold text-xl">Setting</h1>
         <Button onClick={close}>
           <X />
         </Button>
