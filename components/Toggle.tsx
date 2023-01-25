eac8af16e047f1f90fa231d8152a0655a2204b87
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-25 13:55:43 +0100

    pointer

--- components/Toggle.tsx
+++ components/Toggle.tsx
@@ -4,7 +4,7 @@ const Toggle = () => {
   const [on, setOn] = useState(false);
   return (
     <div
-      className="flex items-center w-10 bg-red-50 rounded-full relative h-5"
+      className="flex items-center w-10 bg-red-50 rounded-full relative h-5 cursor-pointer"
       onClick={() => setOn(!on)}
     >
       <div
