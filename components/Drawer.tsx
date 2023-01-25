b433d2052f61af816f9cd4ae8fe7f183e6e68832
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-25 13:02:26 +0100

    fixed

--- components/Drawer.tsx
+++ components/Drawer.tsx
@@ -6,22 +6,18 @@ import { useState } from "react";
 const Drawer = () => {
   const { isOpen, open, close } = useDrawerStore();
   return (
-    <DrawerContext.Provider value={{ isOpen, open, close }}>
-      <div
-        className={`w-72 h-full shadow-2xl absolute bg-[#3e4452] left-0 ${
-          isOpen ? "translate-x-0" : "translate-x-full"
-        } z-[10000000] px-2 transition-all`}
-      >
-        <div className="flex items-center py-1 justify-between">
-          <h1 className="font-semibold text-2xl">
-            {isOpen ? "Settings" : "Blu"}
-          </h1>
-          <Button onClick={close}>
-            <X />
-          </Button>
-        </div>
+    <div
+      className={`w-72 h-full shadow-2xl absolute bg-[#3e4452] right-0 ${
+        isOpen ? "translate-x-0" : "translate-x-full"
+      } z-[10000000] px-2 transition-all`}
+    >
+      <div className="flex items-center py-1 justify-between">
+        <h1 className="font-semibold text-2xl">Setting</h1>
+        <Button onClick={close}>
+          <X />
+        </Button>
       </div>
-    </DrawerContext.Provider>
+    </div>
   );
 };
 
