c9ef27f7109aa82d070832b862caca5e35a6b266
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-25 13:54:45 +0100

    set up

--- components/Toggle.tsx
+++ components/Toggle.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Toggle = () => {
-  return <div>Toggle</div>;
+  const [on, setOn] = useState(false);
+  return (
+    <div className="flex items-center w-10 bg-red-50 rounded-full relative h-5">
+      <div
+        className={`bg-red-500 h-4 w-4 rounded-full absolute ${
+          on ? "right-[2px]" : "left-[2px]"
+        } `}
+      ></div>
+    </div>
+  );
 };
 
 export default Toggle;
