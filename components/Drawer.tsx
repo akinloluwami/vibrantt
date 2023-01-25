2710cbcbee1a1db80e153c98f9d9db98f0f938e4
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-25 12:34:49 +0100

    a lot

--- components/Drawer.tsx
+++ components/Drawer.tsx
@@ -1,22 +1,24 @@
 import Button from "@/component-elements/Button";
+import useDrawerStore from "@/stores/useDrawerStore";
 import { X } from "lucide-react";
 import { useState } from "react";
 
-const Drawer = ({ isOpen }: any) => {
-  const [open, setOpen] = useState(isOpen);
-
+const Drawer = () => {
+  const { isOpen, open, close } = useDrawerStore();
   return (
     <div
       className={`w-72 h-full shadow-2xl absolute bg-[#3e4452] right-0 ${
-        open ? "translate-x-0" : "translate-x-full"
+        isOpen ? "translate-x-0" : "translate-x-full"
       } z-[10000000] px-2`}
     >
       <div className="flex items-center py-1 justify-between">
-        <h1 className="font-semibold text-2xl">Setting</h1>
+        <h1 className="font-semibold text-2xl">
+          {isOpen ? "Settings" : "Blu"}
+        </h1>
         <Button
           onClick={() => {
             console.log("x");
-            setOpen(!isOpen);
+            isOpen ? close() : open();
           }}
         >
           <X />
