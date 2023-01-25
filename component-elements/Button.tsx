1360d08a9ed86b6401bf5c6e3a31e81e2fe19a00
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-25 11:28:30 +0100

    changed padding

--- component-elements/Button.tsx
+++ component-elements/Button.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Button = ({ children, onClick, disabled }: any) => {
   return (
     <button
-      className="hover:bg-slate-700 bg-transparent transition-colors cursor-pointer disabled:opacity-50 disabled:cursor-default mx-1 p-[2px] rounded-full text-lg outline-none border-none"
+      className="hover:bg-slate-700 bg-transparent transition-colors cursor-pointer disabled:opacity-50 disabled:cursor-default mx-1 p-1 rounded-full text-lg outline-none border-none"
       onClick={onClick}
       disabled={disabled}
     >
