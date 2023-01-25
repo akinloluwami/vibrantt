df3561c7bc8092c3a2138bcca92e5c46528507ac
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-25 11:44:14 +0100

    based drawer set up

--- components/Drawer.tsx
+++ components/Drawer.tsx
@@ -1,7 +1,18 @@
+import Button from "@/component-elements/Button";
+import { X } from "lucide-react";
 import React from "react";
 
 const Drawer = () => {
-  return <div>Drawer</div>;
+  return (
+    <div className="w-72 h-full shadow-2xl absolute bg-[#3e4452] right-0 z-[10000000] px-2">
+      <div className="flex items-center py-1 justify-between">
+        <h1 className="font-semibold text-2xl">Setting</h1>
+        <Button>
+          <X />
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Drawer;
