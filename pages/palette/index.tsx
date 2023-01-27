b23485126aa11a780f0b612fa5cae9a7d153ce76
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-27 21:39:39 +0100

    removed more unused code

--- pages/palette/index.tsx
+++ pages/palette/index.tsx
@@ -1,23 +1,9 @@
 import randomColor from "randomcolor";
 import { useContext, useEffect, useState } from "react";
-import { BiCopy } from "react-icons/bi";
-import copy from "copy-to-clipboard";
-import { getLuminosity } from "@/utils/getLuminosity";
-import nearestColor from "nearest-color";
 import colorNameList from "color-name-list";
-import Loading from "../loading";
-import produce from "immer";
-import Header from "@/components/Header";
-import useKeypress from "react-use-keypress";
-import Drawer from "@/components/Drawer";
-import useDrawerStore, { DrawerContext } from "@/stores/useDrawerStore";
 import useColorSpaceStore from "@/stores/useColorSpaceStore";
-import hexToRgb from "@/utils/hexToRgb";
-import hexToHsl from "@/utils/hexToHsl";
 import useToggleStore from "@/stores/useToggleStore";
 import useLuminosityStore from "@/stores/useLuminosityStore";
-import Button from "@/component-elements/Button";
-import { ChevronLeft, ChevronRight, Plus, Settings2, X } from "lucide-react";
 import { useRouter } from "next/router";
 
 export default function Home() {
@@ -29,15 +15,6 @@ export default function Home() {
   const { colorSpace } = useColorSpaceStore();
   const { toggleValue } = useToggleStore();
   const { luminosity } = useLuminosityStore();
-  // const [showTools, setShowTools] = useState(Array(colorCount).fill(false));
-  const [showToolsArray, setShowToolsArray] = useState(
-    Array(colorCount).fill(false)
-  );
-
-  const colors = colorNameList.reduce(
-    (o, { name, hex }) => Object.assign(o, { [name]: hex }),
-    {}
-  );
 
   const router = useRouter();
 
