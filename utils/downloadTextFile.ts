4fb968dedd93c1f584f5b78b3a771f38fb4d9edc
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-30 20:15:22 +0100

    added types

--- utils/downloadTextFile.ts
+++ utils/downloadTextFile.ts
@@ -1,10 +1,15 @@
-const downloadTextFile = () => {
+interface downloadProps {
+  content: string;
+  fileName: string;
+}
+
+const downloadTextFile = ({ content, fileName }: downloadProps) => {
   const element = document.createElement("a");
-  const file = new Blob(["hello"], {
+  const file = new Blob([content], {
     type: "text/plain",
   });
   element.href = URL.createObjectURL(file);
-  element.download = "myFile.txt";
+  element.download = `${fileName}.txt`;
   document.body.appendChild(element);
   element.click();
 };
