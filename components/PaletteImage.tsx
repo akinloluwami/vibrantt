eb4f52be0ea3afb92068de74d7285e9a8be5a6b8
bossoncode <akinkunmiisrael41@gmail.com>
2023-02-17 15:32:10 +0100

    ___

--- components/PaletteImage.tsx
+++ components/PaletteImage.tsx
@@ -3,31 +3,30 @@ import React from "react";
 const PaletteImage = ({ palette, ref }: any) => {
   return (
     <div
-      className="absolute w-96 z-[999999999999999999] py-3 h-96 bg-[#1a1a1a] flex items-center justify-between flex-col translate-x-[-1100px]"
+      className="absolute w-96 z-[999999999999999999] h-96 bg-[#1a1a1a] flex items-center  flex-col translate-x-[-1100px]"
       ref={ref}
     >
-      <div className="">
+      <div className="h-[10%] flex items-center justify-center">
         <img
           src="https://res.cloudinary.com/xing0x/image/upload/v1674131509/VIBRANTwbgggw_w6j7m9.png"
-          className="w-32"
+          className="w-24"
           alt=""
         />
       </div>
-      <div className="flex-2 flex">
-        {palette.slice(0, 5).map((color: string) => (
-          <div className="flex flex-col items-center">
+      <div className="h-[83%] flex w-full flex-col bg-red-400">
+        {palette.map((color: string) => (
+          <div className="w-full flex h-full">
             <div
-              className="h-14 w-14 rounded-full mx-2"
+              className="w-full flex items-center "
               style={{ backgroundColor: color }}
-            ></div>
-            <h1 className="font-semibold text-sm mt-2">
-              {color.toUpperCase()}
-            </h1>
+            >
+              <h1 className="font-semibold  ml-4">{color.toUpperCase()}</h1>
+            </div>
           </div>
         ))}
       </div>
-      <div className="">
-        <h1>Generate by vibrantt.co</h1>
+      <div className="w-full flex items-center justify-center h-[7%]">
+        <h1 className="text-xs">Generate by vibrantt.co</h1>
       </div>
     </div>
   );
